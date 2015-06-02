5f5a6dd7f5647c8dbfd3b847418b5116ae7e57a8
Michael Burns <michael.burns@rackspace.com>
2015-06-01 22:48:32 -0700

    rubocop -a

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-maintainer       'Rackspace'
+maintainer 'Rackspace'
 maintainer_email 'dave.king@rackspace.com'
-license          'Apache 2.0'
-name             'cleanup'
-description      'Installs/Configures cleanup'
+license 'Apache 2.0'
+name 'cleanup'
+description 'Installs/Configures cleanup'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version '0.0.1'
