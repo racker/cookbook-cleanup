c2b97ef938cda4be22fcdec328ec13121aba681b
Michael Burns <michael.burns@rackspace.com>
2015-07-04 22:44:05 -0700

    add integration tests and fixture cookbook

---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,20 @@
+# encoding: UTF-8
+# License:: Apache License, Version 2.0
+#
+
+require_relative 'spec_helper'
+
+describe file('/cleanup/foo') do
+  it { should_not be_file }
+end
+
+describe file('/cleanup/bar') do
+  it { should_not be_file }
+end
+
+describe file('/cleanup/baz') do
+  it { should be_file }
+end
+describe file('/cleanup/quz') do
+  it { should be_file }
+end
