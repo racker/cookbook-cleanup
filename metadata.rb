0337839667295e32fdb3fb1cb821d75e1a856943
Michael Burns <michael@mirwin.net>
2018-02-11 03:12:23 -0800

    add supported platforms

--- metadata.rb
+++ metadata.rb
@@ -8,3 +8,7 @@ version '0.2.0'
 
 source_url 'https://github.com/racker/cookbook-cleanup' if respond_to?(:source_url)
 issues_url 'https://github.com/racker/cookbook-cleanup/issues' if respond_to?(:issues_url)
+
+supports 'centos'
+supports 'debian'
+supports 'ubuntu'
