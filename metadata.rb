85f024a88443e0ab0538cd1d70000d94db83a718
Michael Burns <michael@mirwin.net>
2018-02-11 03:11:09 -0800

    [lint] add new metadata

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,7 @@ license 'Apache 2.0'
 name 'cleanup'
 description 'Installs/Configures cleanup'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.2.0'
+
+source_url 'https://github.com/racker/cookbook-cleanup' if respond_to?(:source_url)
+issues_url 'https://github.com/racker/cookbook-cleanup/issues' if respond_to?(:issues_url)
