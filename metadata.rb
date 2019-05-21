a62e3b0d98c155494203efa275c3f216e2f6c730
Elnur Mammadov <1993slackware@gmail.com>
2019-05-21 13:37:50 +0300

    Added windows os support

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ license 'Apache 2.0'
 name 'cleanup'
 description 'Installs/Configures cleanup'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.1'
+version '0.3.1'
 
 source_url 'https://github.com/racker/cookbook-cleanup' if respond_to?(:source_url)
 issues_url 'https://github.com/racker/cookbook-cleanup/issues' if respond_to?(:issues_url)
@@ -12,3 +12,4 @@ issues_url 'https://github.com/racker/cookbook-cleanup/issues' if respond_to?(:i
 supports 'centos'
 supports 'debian'
 supports 'ubuntu'
+supports 'windows'
